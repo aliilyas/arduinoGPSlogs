d6802cd4af152005c1b045748b9799c2ce97db8e
aliilyas <alikhojayev@gmail.com>
2013-01-17 02:30:20 +0600

    some improvements

--- sketch_jan15a.ino
+++ sketch_jan15a.ino
@@ -94,12 +94,12 @@ void composeAndOutput(String input){
   String longitude = splitString(input,5);
   String latitude = splitString(input,3);
   String gDirection = splitString(input,4)+"/"+splitString(input,6);
-  String speedInKnots = splitString(input,7);
+  String knots = splitString(input,7);
   String course = splitString(input,8);
   String variation = splitString(input,10);
   String delimiter = " | ";
 
-  output = timestamp + ":"+date + delimiter + " Long : " + longitude +delimiter + " Lat : " +latitude +delimiter+ " Speed : " +speedInKnots; 
+  output = timestamp + ":"+date + delimiter + " Long : " + longitude +delimiter + " Lat : " +latitude +delimiter+ " Speed : " + knots; 
 
   myFile = SD.open("newa.txt", FILE_WRITE);
   myFile.println(output);
@@ -130,55 +130,9 @@ String splitString(String s, int index){
   return rs;
 }
 
-
-
-template<typename Data>
-class Vector {
-  size_t d_size; // Stores no. of actually stored objects
-  size_t d_capacity; // Stores allocated capacity
-  Data *d_data; // Stores data
-public:
-  Vector() : 
-  d_size(0), d_capacity(0), d_data(0) {
-  }; // Default constructor
-  Vector(Vector const &other) : 
-  d_size(other.d_size), d_capacity(other.d_capacity), d_data(0) {
-    d_data = (Data *)malloc(d_capacity*sizeof(Data)); 
-    memcpy(d_data, other.d_data, d_size*sizeof(Data)); 
-  }; // Copy constuctor
-  ~Vector() { 
-    free(d_data); 
-  }; // Destructor
-  Vector &operator=(Vector const &other) { 
-    free(d_data); 
-    d_size = other.d_size; 
-    d_capacity = other.d_capacity; 
-    d_data = (Data *)malloc(d_capacity*sizeof(Data)); 
-    memcpy(d_data, other.d_data, d_size*sizeof(Data)); 
-    return *this; 
-  }; // Needed for memory management
-  void push_back(Data const &x) { 
-    if (d_capacity == d_size) resize(); 
-    d_data[d_size++] = x; 
-  }; // Adds new value. If needed, allocates more space
-  size_t size() const { 
-    return d_size; 
-  }; // Size getter
-  Data const &operator[](size_t idx) const { 
-    return d_data[idx]; 
-  }; // Const getter
-  Data &operator[](size_t idx) { 
-    return d_data[idx]; 
-  }; // Changeable getter
-private:
-  void resize() { 
-    d_capacity = d_capacity ? d_capacity*2 : 1; 
-    Data *newdata = (Data *)malloc(d_capacity*sizeof(Data)); 
-    memcpy(newdata, d_data, d_size * sizeof(Data)); 
-    free(d_data); 
-    d_data = newdata; 
-  };// Allocates double the old space
-};
+float knotToKilometers(float knot){
+  return knot*1.852;
+}
 
 
 /***********  NMEA message  *****************************************/
